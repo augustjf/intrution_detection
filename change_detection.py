f910c119b46d44d61b76ac0611ff5fcb82881f3b
August Fors <augustjf@stud.ntnu.no>
2023-12-29 12:35:56 +0100

    Added writing to textfile and improved algorithm

--- change_detection.py
+++ change_detection.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 # out = cv2.VideoWriter(
 #     save_name,
@@ -10,7 +11,7 @@ import numpy as np
 class ChangeDetection:
     def __init__(self):
         self.input_path = 'rilevamento-intrusioni-video.wm'
-        self.consecutive_frames = 4
+        self.consecutive_frames = 1
         self.learning_rate = -1
         self.cap = cv2.VideoCapture(self.input_path)
         self.current_frame = None
@@ -19,16 +20,21 @@ class ChangeDetection:
         self.init_frame = None
         self.motion_mask = None
         self.init_frame_ave = 0.0
+        self.total_frames = 0
+        self.frame_num = 0
+        self.contours = []
         self.frame_width = int(self.cap.get(3))
         self.frame_height = int(self.cap.get(4))
         self.histogram = None
         self.thresh_val = 0
+        self.background_update = 0
+        self.frames_in_background_ave = 60
         self.init_thresh_split = 0.8
         self.prop_weight = 1.5
         self.thresh_split = 0
+        self.info_text = open("change_detection.txt", "a")
         self.num_pixels = self.frame_height*self.frame_width
         self.MoG_obj = cv2.createBackgroundSubtractorMOG2()
-    
 
 
     def set_histogram(self):
@@ -39,15 +45,12 @@ class ChangeDetection:
         self.histogram = vec
 
     def find_threshold(self):
-        split_val = self.num_pixels*self.thresh_split
-        iterate_dark_vals = 0
-        i = 0
-        while iterate_dark_vals < split_val:
-            if i==255:
-                break
-            iterate_dark_vals += self.histogram[i]
-            i += 1
-        self.thresh_val = i
+        thresh_val = 0
+        for i in range(10, 255):
+            dx = self.histogram[i-2] - self.histogram[i]
+            if dx > 800:
+                thresh_val = i
+        self.thresh_val = thresh_val+2
 
     def display_hist(self):
         hist_height = max(self.histogram)//20 
@@ -64,13 +67,13 @@ class ChangeDetection:
 
     def find_thresh_split(self):
         ave = np.average(self.current_frame)
-        print(self.init_frame_ave/ave)
         prop = ave/self.init_frame_ave
         self.thresh_split = self.init_thresh_split*prop
 
-    def get_video_background(self):
+    def get_background(self):
         frames = []
-        for idx in range(50):
+        num = self.frames_in_background_ave
+        for idx in range(self.frame_num, self.frame_num+num):
             # set the frame id to read that particular frame
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, idx)
             _, rgb_frame = self.cap.read()
@@ -82,55 +85,85 @@ class ChangeDetection:
     def get_video_background_MoG(self):
         self.motion_mask = self.MoG_obj.apply(self.current_frame, self.learning_rate)
         bg = self.MoG_obj.getBackgroundImage()
-        self.background = bg
+        self.background = cv2.cvtColor(bg, cv2.COLOR_RGB2GRAY)
+
+    def write_info(self):
+        frame_num = repr(self.frame_num)
+        num_objects = repr(len(self.contours))
+        self.info_text = open("change_detection.txt", "a")
+        self.info_text.write("Frame number: " + frame_num + '\n')
+        self.info_text.write("Num objects detected: " + num_objects + '\n')
+        for i, cnt in enumerate(self.contours):
+            num = repr(i+1)
+            area = repr(cv2.contourArea(cnt))
+            M = cv2.moments(cnt)
+            cx = repr(M['m10']//M['m00'])
+            cy = repr(M['m01']//M['m00'])
+            if cv2.contourArea(cnt) > 500: #Arbitrary large value
+                classification = repr('Person')
+            else:
+                classification = repr('Other')
+            self.info_text.write("Object number: " + num + " Area: " + area + " Center of mass: (" + cx + "," + cy + ") Classification: " + classification + "\n")
+        self.info_text.write('\n')
+        self.info_text.close()
 
 def main():
+    os.remove("change_detection.txt")
     cd = ChangeDetection()
-    cd.get_video_background()
-    _ = cd.MoG_obj.apply(cd.background, cd.learning_rate)
+    cd.get_background()
+    prop_id = int(cv2.CAP_PROP_FRAME_COUNT)
+    cd.total_frames = int(cv2.VideoCapture.get(cd.cap, prop_id))
+    print(cd.total_frames)
+    
     cd.cap.set(cv2.CAP_PROP_POS_FRAMES, 1)
     _, cd.init_frame = cd.cap.read()
     cd.init_frame_ave = np.average(cd.init_frame)
     frame_count = 0
     while cd.cap.isOpened():
         ret, cd.current_frame = cd.cap.read()
+        cd.frame_num += 1
         if ret == True:
-            cd.current_frame = cv2.cvtColor(cd.current_frame, cv2.COLOR_RGB2GRAY)
-            # cv2.imshow('background', cd.background)
-            # cv2.waitKey(0)
-            # cv2.imshow('mask', cd.motion_mask)
-            # cv2.waitKey(0)
-            frame_count += 1
             original_frame = cd.current_frame.copy()
-            
-            if frame_count%cd.consecutive_frames == 0 or frame_count == 1:
-                frame_list = []
-
+            cd.current_frame = cv2.cvtColor(cd.current_frame, cv2.COLOR_RGB2GRAY)
+            frame_count += 1
+            if cd.frame_num % cd.frames_in_background_ave == 0 and cd.total_frames - cd.frame_num > cd.frames_in_background_ave:
+                cd.get_background()
+                print('New background')
+            print(cd.frame_num)
             cd.diff_frame = cv2.absdiff(cd.current_frame, cd.background)
+            cv2.imshow('Background Frame', cd.background)
+            cv2.waitKey(0) 
             cd.set_histogram()
             cd.find_thresh_split()
             cd.find_threshold()           
-            print(f'thresh split {cd.thresh_split}')
-            print(f'thresh val {cd.thresh_val}')
             cd.display_hist()
             _, thresh_frame = cv2.threshold(cd.diff_frame, cd.thresh_val, 255, cv2.THRESH_BINARY)
-            cv2.imshow('Thresh Frame', thresh_frame)
-            cv2.waitKey(0)       
-            opened_frame = cv2.morphologyEx(thresh_frame, cv2.MORPH_OPEN, np.ones((2,2), np.uint8))
-            closed_frame = cv2.morphologyEx(opened_frame, cv2.MORPH_CLOSE, np.ones((20,20), np.uint8))
-            cv2.imshow('Closed Frame', closed_frame)
-            cv2.waitKey(0)
-            dialated_frame = cv2.dilate(thresh_frame, None, iterations=2)
-            frame_list.append(dialated_frame)
-            contours, hierarchy = cv2.findContours(closed_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            for i in range(len(contours)):
-                cv2.drawContours(original_frame, contours, i, (0,0,225), 3)
-
-            # if len(frame_list) == consecutive_frames:
-            #     sum_frames = sum(frame_list)
-                
             cv2.imshow('Diff Frame', cd.diff_frame)
+            cv2.waitKey(0)  
+
+            kernel1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2,2))
+            kernel5 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
+    
+            opened_frame = cv2.morphologyEx(thresh_frame, cv2.MORPH_OPEN, kernel1)
+            for i in range(2,4):
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (i*2,i*2))
+                closed_frame = cv2.morphologyEx(opened_frame, cv2.MORPH_CLOSE, kernel)
+                opened_frame = cv2.morphologyEx(closed_frame, cv2.MORPH_OPEN, kernel)
+
+            closed_frame = cv2.morphologyEx(opened_frame, cv2.MORPH_CLOSE, kernel5)
+            cv2.imshow('Thresh Frame', thresh_frame)
             cv2.waitKey(0)
+            
+            cd.contours, _ = cv2.findContours(closed_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cd.contours = [cnt for cnt in cd.contours if cv2.contourArea(cnt) > 150]
+            for i in range(len(cd.contours)):
+                cv2.drawContours(original_frame, cd.contours, i, (0,0,225), thickness=cv2.FILLED)
+                #cv2.drawContours(original_frame, hulls, i, (0,255,0), 3)
+
+                
+            cv2.imshow('Res', original_frame)
+            cv2.waitKey(0)
+            cd.write_info()
             #out.write(frame)
         else:
             break
